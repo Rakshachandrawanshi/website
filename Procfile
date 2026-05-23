d52e1855ab555b3093ca2b92ae3b39bf5dd984cd
Raksha <rakshachandrawanshi6@gmail.com>
2026-05-23 16:08:09 +0530

    Bind Render startup to service port

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate && gunicorn company.wsgi
+web: python manage.py migrate && gunicorn company.wsgi --bind 0.0.0.0:$PORT
--- company/wsgi.py
+++ company/wsgi.py
@@ -9,12 +9,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'company.settings')
 
 application = get_wsgi_application()
-
-if os.environ.get("DJANGO_AUTO_MIGRATE", "True").lower() in ("1", "true", "yes", "on"):
-    call_command("migrate", interactive=False, verbosity=1)
